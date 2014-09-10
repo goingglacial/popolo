d958feb1618a74c7d4dc51c9a7f10b65c3842621
Annie Kramer <anniekramer@dropbox.com>
2014-09-10 14:40:07 -0700

    Add unicode method to model

--- mysite/popolo/models.py
+++ mysite/popolo/models.py
@@ -4,3 +4,10 @@ class Pops (models.Model):
     city = models.CharField(max_length=40)
     state = models.CharField(max_length=10)
     pop = models.CharField(max_length=40)
+
+    def __unicode__(self):
+        return self.city
+    def __unicode__(self):
+        return self.state
+    def __unicode__(self):
+        return self.pop
