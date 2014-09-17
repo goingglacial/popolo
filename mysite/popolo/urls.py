27f556b67ef88e7eab513e23e6082b96b397a98f
Annie Kramer <anniekramer@dropbox.com>
2014-09-17 11:36:02 -0700

    Configure Texas pops view

--- mysite/popolo/urls.py
+++ mysite/popolo/urls.py
@@ -4,5 +4,7 @@ from popolo import views
 urlpatterns = patterns('',
     url(r'^index$', views.index, name='index'),
     url(r'^results$', views.results, name='results'),
-    url(r'^random$', views.random, name='random')
+    url(r'^random$', views.random, name='random'),
+    url(r'^fifty$', views.fifty, name='fifty'),
+    url(r'^texas$', views.texas, name='texas'),
 )
--- mysite/popolo/views.py
+++ mysite/popolo/views.py
@@ -19,3 +19,17 @@ def random(request):
     context = RequestContext(request, {
         'all_entries': all_entries})
     return HttpResponse(template.render(context))
+
+def fifty(request):
+    all_entries = Popul.objects.all()[:50]
+    template = loader.get_template('50pops.html')
+    context = RequestContext(request, {
+        'all_entries': all_entries})
+    return HttpResponse(template.render(context))
+
+def texas(request):
+    all_entries = Popul.objects.filter(state='TX')
+    template = loader.get_template('txpops.html')
+    context = RequestContext(request, {
+        'all_entries': all_entries})
+    return HttpResponse(template.render(context))
