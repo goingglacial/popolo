64c19a37c25cc75c4ae327a7cbf82b2f090d5ce5
Annie Kramer <anniekramer@dropbox.com>
2014-09-24 15:31:20 -0700

    Add lineage to installed apps

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'popolo'
+    'popolo',
+    'lineage'
 )
 
 MIDDLEWARE_CLASSES = (
